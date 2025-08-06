7d873de53224249d18c049f7f4f361d105d8cc10
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2025-08-06 21:01:06 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/login';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";import Login from './pages/login';
 import Signup from './pages/signup';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
