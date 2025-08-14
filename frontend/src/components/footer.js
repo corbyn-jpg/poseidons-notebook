9055fe11afc26dd6b6274762c76241f82b58c294
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2025-08-14 12:40:00 +0200

    Footer styling and adding the socials

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import FackebookIcon from '../assets/facebook.png';
+import TwitterIcon from '../assets/twitter.png'; 
+import InstagramIcon from '../assets/instagram.png';
+import TiktokIcon from '../assets/tik-tok.png';
+import '../styles/footer.css';
 
 const Footer = () => {
   return (
@@ -16,9 +21,9 @@ const Footer = () => {
       <div className="footer-section">
         <h4>Resources</h4>
         <ul>
-          <li><Link to="/guides">Marine Life Guides</Link></li>
-          <li><Link to="/tips">Conservation Tips</Link></li>
-          <li><Link to="/partners">Research Partners</Link></li>
+          <li><a href='https://kirstenboschbookshop.co.za/product-category/books/marine/' rel="noreferrer" target='_blank'>Marine Life Guides</a></li>
+          <li><a href='https://www.oceans-research.com/protecting-oceans-10-ways-to-contribute/' rel="noreferrer" target='_blank'>Conservation Tips</a></li>
+          <li><a href='https://saambr.org.za/oceanographic-research-institute-ori/' rel="noreferrer" target='_blank'>Research Partners</a></li>
         </ul>
       </div>
 
@@ -28,6 +33,16 @@ const Footer = () => {
           <li><Link to="/terms">Terms of Service</Link></li>
           <li><Link to="/privacy">Privacy Policy</Link></li>
           <li><Link to="/cookies">Cookie Policy</Link></li>
+        </ul>
+      </div>
+
+      <div className="footer-section">
+        <h4>Socials</h4>
+        <ul>
+          <li><a href='https://www.facebook.com/theopenwindow/' rel="noreferrer" target='_blank'><img src={FackebookIcon} alt="Facebook" className="footer-icon" /></a></li>
+          <li><a href='https://x.com/open_window_?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor' rel="noreferrer" target='_blank'><img src={TwitterIcon} alt="Twitter" className="footer-icon" /></a></li>
+          <li><a href='https://www.instagram.com/openwindowinstitute/?hl=en' rel="noreferrer" target='_blank'><img src={InstagramIcon} alt="Instagram" className="footer-icon" /></a></li>
+          <li><a href='https://www.tiktok.com/@open_window_' rel="noreferrer" target='_blank'><img src={TiktokIcon} alt="Tiktok" className="footer-icon" /></a></li>
         </ul>
       </div>
     </footer>
