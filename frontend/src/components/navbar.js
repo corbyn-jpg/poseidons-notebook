cc47cf098ff485b4e52b55e89f305a76857f6095
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2025-09-23 15:33:01 +0200

    Navbar styling

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -21,25 +21,37 @@ const Navbar = () => {
         </div>
       </Link>
       <nav>
-        <ul>
+        <ul className="animated-nav">
           <li>
-            <Link to="/species">
-              <img src={turtleIcon} alt="Species" className="nav-icon" />
+            <Link to="/species" className="nav-link">
+              <span className="icon">
+                <img src={turtleIcon} alt="Species" className="nav-icon" />
+              </span>
+              <span className="title">Species</span>
             </Link>
           </li>
           <li>
-            <Link to="/sightings">
-              <img src={seashellIcon} alt="Sightings" className="nav-icon" />
+            <Link to="/sightings" className="nav-link">
+              <span className="icon">
+                <img src={seashellIcon} alt="Sightings" className="nav-icon" />
+              </span>
+              <span className="title">Sightings</span>
             </Link>
           </li>
           <li>
-            <Link to="/contributions">
-              <img src={ContributionsIcon} alt="Contributions" className="nav-icon" />
+            <Link to="/contributions" className="nav-link">
+              <span className="icon">
+                <img src={ContributionsIcon} alt="Contributions" className="nav-icon" />
+              </span>
+              <span className="title">Contributions</span>
             </Link>
           </li>
           <li>
-            <Link to="/signup">
-              <img src={diverIcon} alt="Signup" className="nav-icon" />
+            <Link to="/signup" className="nav-link">
+              <span className="icon">
+                <img src={diverIcon} alt="Signup" className="nav-icon" />
+              </span>
+              <span className="title">Logout</span>
             </Link>
           </li>
         </ul>
