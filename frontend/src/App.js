f7488f204c6fe326aa2337ba735f7688fb7d1953
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2025-10-26 20:40:39 +0200

    Revert "Add admin dashboard and route for admin users"
    This reverts commit 3ffe9f5fcbe2756d061b7eb1b550e8d44dda3ffe.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import HomePage from "./pages/homePage";
 import SpeciesPage from './pages/speciesPage';
 import SightingsPage from "./pages/sightingsPage";
 import ContributionsPage from "./pages/contributionsPage";
-import AdminPage from './pages/adminPage';
 
 function App() {
   return (
@@ -20,7 +19,6 @@ function App() {
           <Route path="/species" element={<SpeciesPage />} />
           <Route path="/sightings" element={<SightingsPage />} />
           <Route path="/contributions" element={<ContributionsPage />} />
-          <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </Router>
     </div>
